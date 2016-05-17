c21f31017ba5b28eddae10630efe7bf25c2e0736
elektra8875 <clg021558@gmail.com>
2016-05-16 23:00:23 -0400

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,32 @@
+document.write("tesing my for while loops");
+
+function IbisEatDonuts() {
+	console.log("Ibis enjoyed a powered vanilla creme donut. ");
+	console.log("Ibis enjoyed a powered vanilla frosted with sprinkles. ");
+	
+}
+
+IbisEatDonuts();
+
+for (var a = 1; a <=30; a++) {
+	console.log(a);
+}
+
+for (var i = 1; i <= 20 ;i++) {
+  console.log("6 x " + i + " = " + 6*i)
+//6 * 1
+
+}
+
+function x() {
+	alert("x+y");
+}
+x();
+
+function myFunction () {
+	console.log("this sucks");
+	
+}
+
+myFunction();
+ 
